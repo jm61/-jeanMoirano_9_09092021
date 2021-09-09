53901784e3cdd482db0dd2357a18de4d54ae55c0
jm61 <jean.moirano@gmail.com>
2021-09-09 17:53:00 +0200

    initial shot

---
+++ src/app/format.js
@@ -0,0 +1,23 @@
+export const formatDate = (dateStr) => {
+  // date string empty fix
+  if (!dateStr) {
+    return ''
+  }
+  const date = new Date(dateStr)
+  const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
+  const mo = new Intl.DateTimeFormat('fr', { month: 'long' }).format(date)
+  const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
+  const month = mo.charAt(0).toUpperCase() + mo.slice(1)
+  return `${parseInt(da)} ${month.substr(0,4)}. ${ye.toString()}`
+}
+ 
+export const formatStatus = (status) => {
+  switch (status) {
+    case "pending":
+      return "En attente"
+    case "accepted":
+      return "Accepté"
+    case "refused":
+      return "Refused"
+  }
+}
